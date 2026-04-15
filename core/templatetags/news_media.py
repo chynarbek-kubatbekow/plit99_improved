367052e7735a60cf7c33462d23c600c0893a3166
Chynar <chynarbek2804@gmail.com>
2026-04-15 19:15:48 +0600

    LAST1

---
+++ core/templatetags/news_media.py
@@ -0,0 +1,19 @@
+from django import template
+from django.templatetags.static import static
+
+register = template.Library()
+
+
+NEWS_STATIC_COVERS = {
+    '3b871fdd': 'img/news-covers/news-career-orientation.jpg',
+    '7da9f42f': 'img/news-covers/news-sarmerden-nooruz.jpg',
+    'no99-21': 'img/news-covers/news-school-21.jpg',
+    '2-3': 'img/news-covers/news-german-hackathon.jpg',
+}
+
+
+@register.filter
+def news_static_cover(news):
+    slug = getattr(news, 'slug', '')
+    path = NEWS_STATIC_COVERS.get(slug)
+    return static(path) if path else ''
